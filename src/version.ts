eb186881ec8d224c81d977359c40a03e1b49710f
uvinduachinthya <uvinduachinthya@gmail.com>
2025-12-24 19:07:33 +0530

    Update: version notes.

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,6 @@
-export const APP_VERSION = '1.0.7 Beta';
-export const RELEASE_DATE = 'December 23, 2025';
+export const APP_VERSION = '1.0.8 Beta';
+export const RELEASE_DATE = 'December 24, 2025';
 export const RELEASE_NOTES = [
-    "Added a \"What's New\" popup to show update details.",
-    "The About section now dynamically displays the current version.",
-    "Internal improvements for smoother updates."
+    "Note renaming bug fixed.",
+    "Performance improvements for note creation, renaming, and deletion."
 ];
